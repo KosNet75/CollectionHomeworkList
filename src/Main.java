cfe3981c78e6594479bb838ba34801b821f40a46
KosNet75 <nemokonstantin@gmail.com>
2022-08-12 16:40:34 +0700

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static <Integernt> void main(String[] args) {
+  public static void main(String[] args) {
 
     List<String> list = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
